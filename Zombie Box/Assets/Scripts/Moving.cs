648889db3a889ed4f84544dcd2326a31b32041b0
vanyaadev <49350015+vanyaadev@users.noreply.github.com>
2019-05-02 02:51:04 +0200

    added joystick

---
+++ Zombie Box/Assets/Scripts/Moving.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public Joystick joystick;
+    
+    
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+                
+    }
+
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        var characterController = GetComponent<CharacterController>();
+
+        transform.SetPositionAndRotation(new Vector3(transform.position.x + joystick.Horizontal * 100f * Time.deltaTime,
+            transform.position.y, transform.position.z + joystick.Vertical * 100f * Time.deltaTime),new Quaternion());
+
+
+        /*rigidbody.velocity = new Vector3(joystick.Horizontal * 100f*Time.deltaTime,
+                                         rigidbody.velocity.y,
+                                         joystick.Vertical * 100f*Time.deltaTime);*/
+    }
+}
