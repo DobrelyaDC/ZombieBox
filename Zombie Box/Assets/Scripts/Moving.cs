5c5d0ca02556b95f749d4b68dddfb7a9f43f55c5
daniel <squeez.daniil6@gmail.com>
2019-05-04 23:27:40 +0300

    Change speed

--- Zombie Box/Assets/Scripts/Moving.cs
+++ Zombie Box/Assets/Scripts/Moving.cs
@@ -17,8 +17,8 @@ public class Moving : MonoBehaviour
         
         var characterController = GetComponent<CharacterController>();
 
-        transform.SetPositionAndRotation(new Vector3(transform.position.x + joystick.Horizontal * 100f * Time.deltaTime,
-            transform.position.y, transform.position.z + joystick.Vertical * 100f * Time.deltaTime),new Quaternion());
+        transform.SetPositionAndRotation(new Vector3(transform.position.x + joystick.Horizontal * 20f * Time.deltaTime,
+            transform.position.y, transform.position.z + joystick.Vertical * 20f * Time.deltaTime),new Quaternion());
 
 
         
